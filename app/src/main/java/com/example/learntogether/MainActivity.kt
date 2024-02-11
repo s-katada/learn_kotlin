a42dd0dd7c3205d69dd7fd8b25c6ec13dff4396e
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2024-02-11 11:32:03 +0900

    finish completed app

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -4,20 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
@@ -26,51 +27,53 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearnTogetherTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    color = Color.White,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    HeaderImage()
-                }
+            Surface(
+                color = colorResource(id = R.color.white),
+                modifier = Modifier.fillMaxSize()
+            ) {
+                RenderScreen()
             }
         }
     }
 }
 
 @Composable
-fun HeaderImage() {
-    val image = painterResource(id = R.drawable.bg_compose_background)
-    Column {
-        Image(painter = image, contentDescription = null)
-        Title()
-        SubTitle()
-        Content()
+fun RenderScreen() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    )
+    {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CompletedImage()
+            CompletedText()
+            ComplimentaryText()
+        }
     }
 }
 
 @Composable
-fun Title() {
+fun CompletedImage() {
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Image(painter = image, contentDescription = null)
+}
+
+@Composable
+fun CompletedText() {
     Text(
-        text = stringResource(id = R.string.title),
-        fontSize = 24.sp,
-        modifier = Modifier.padding(16.dp)
+        text = stringResource(id = R.string.completed_text),
+        modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+        fontWeight = FontWeight.Bold
     )
 }
 
 @Composable
-fun SubTitle() {
+fun ComplimentaryText() {
     Text(
-        text = stringResource(id = R.string.subtitle),
-        modifier = Modifier.padding(horizontal = 16.dp)
-    )
-}
-
-@Composable
-fun Content() {
-    Text(
-        text = stringResource(id = R.string.content),
-        modifier = Modifier.padding(16.dp)
+        text = stringResource(id = R.string.complimentary_text),
+        fontSize = 16.sp
     )
 }
