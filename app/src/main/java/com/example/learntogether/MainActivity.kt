f4f52625fa8567515f123652c0f58b1eb0992a2d
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2024-02-11 14:14:49 +0900

    finish compose Quadrant app

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,12 +18,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,49 +30,51 @@ class MainActivity : ComponentActivity() {
                 color = colorResource(id = R.color.white),
                 modifier = Modifier.fillMaxSize()
             ) {
-                RenderScreen()
+                QuadrantScreen()
             }
         }
     }
 }
 
 @Composable
-fun RenderScreen() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    )
-    {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            CompletedImage()
-            CompletedText()
-            ComplimentaryText()
+fun QuadrantScreen() {
+    Row {
+        Column {
+            QuarterElement(0xFFEADDFF.toInt(), 0.5f, 0.5f, R.string.text_composable_title, R.string.text_composable_content)
+            QuarterElement(0xFFB69DF8.toInt(), 0.5f,  1f, R.string.row_composable_title, R.string.row_composable_content)
+        }
+        Column {
+            QuarterElement(0xFFD0BCFF.toInt(), 1f, 0.5f, R.string.image_composable_title, R.string.image_composable_content)
+            QuarterElement(0xFFF6EDFF.toInt(), 1f,  1f, R.string.column_composable_title, R.string.column_composable_content)
         }
     }
 }
 
 @Composable
-fun CompletedImage() {
-    val image = painterResource(id = R.drawable.ic_task_completed)
-    Image(painter = image, contentDescription = null)
-}
-
-@Composable
-fun CompletedText() {
-    Text(
-        text = stringResource(id = R.string.completed_text),
-        modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
-        fontWeight = FontWeight.Bold
-    )
-}
-
-@Composable
-fun ComplimentaryText() {
-    Text(
-        text = stringResource(id = R.string.complimentary_text),
-        fontSize = 16.sp
-    )
+fun QuarterElement(
+    color: Int,
+    maxWidth: Float,
+    maxHeight: Float,
+    titleId: Int,
+    contentId: Int
+) {
+    Surface(
+        color = Color(color),
+        modifier = Modifier
+            .fillMaxWidth(maxWidth)
+            .fillMaxHeight(maxHeight)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = stringResource(titleId),
+                modifier = Modifier.padding(bottom = 16.dp),
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = stringResource(contentId))
+        }
+    }
 }
